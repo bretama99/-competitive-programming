c87edccede3bada2c78dfb6cef3ecfd3d9419dc8
brhane <bretama99@gmail.com>
2022-08-11 19:43:32 +0300

    1985. Find the Kth Largest Integer in the Array file added

---
+++ FindtheKthLargestIntegerintheArray.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kthLargestNumber(self, nums, k):
+        """
+        :type nums: List[str]
+        :type k: int
+        :rtype: str
+        """
+        nums.sort(key=int)
+        return nums[len(nums)-k]
