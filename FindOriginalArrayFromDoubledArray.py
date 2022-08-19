a093192ae886fbff610f2943fca221c1a54e9847
brhane <bretama99@gmail.com>
2022-08-19 08:46:30 +0300

    add worked files

---
+++ FindOriginalArrayFromDoubledArray.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findOriginalArray(self, changed):
+        """
+        :type changed: List[int]
+        :rtype: List[int]
+        """
+        n=len(changed)
+        if n%2:
+            return []
+        count = Counter(changed)
+        changed.sort()
+        ans = []
+        for num in changed:
+            if num==0 and count[num]>=2:
+                count[num]-=2
+                ans.append(num)
+            elif num!=0 and count[num] and count[num*2]:
+                count[num]-=1
+                count[num*2]-=1
+                ans.append(num)
+        return ans if len(ans)==n//2 else []
