5caacd437b1145b44949bdac577509564a585abd
bretama99 <49144732+bretama99@users.noreply.github.com>
2022-09-07 15:01:08 +0300

    Create Daily Temperatures
    Create Daily Temperatures

---
+++ DailyTemperatures.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0]*len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]]<temperatures[i]:
+                past_day = stack.pop()
+                answer[past_day] = i-past_day
+            stack.append(i)
+        return answer
+        
